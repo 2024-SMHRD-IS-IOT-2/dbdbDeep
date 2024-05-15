e099091548748260da245c10df622ff730406cff
Jay Kim <jaykim.coding@gmail.com>
2024-05-15 20:10:49 +0900

    outputHandler. Done. AudioDelay problem...

--- raspi_zzanggu/main.py
+++ raspi_zzanggu/main.py
@@ -19,69 +19,82 @@ def main():
                                sensitivity=float(os.environ['PORCUPINE_SENSITIVITY']))  
     # taskClassifier = TaskClassifier()  ## 분류 llm 객체
     # convGen = ConvGenThread(event)  ## 대화생성 llm 객체
-    # generateOutputAudio = GenerateOutputAudioThread(event) ## 출력오디오 생성 객체
-    # playAudio = PlayAudioThread(event)  ## 오디오 출력 객체
+    generateOutputAudio = GenerateOutputAudioThread(event=event,
+                                                    actor_id=os.environ['TYPECAST_ACTOR_ID'], 
+                                                    api_key=os.environ['TYPECAST_API_KEY']) ## 출력오디오 생성 객체
+    playAudio = PlayAudioThread(event)  ## 오디오 출력 객체
 
     # emotionModel = EmotionModelThread(event)
 
     # convGen.set_output_queue(generateOutputAudio.input_queue)
-    # generateOutputAudio.set_output_queue(playAudio.input_queue)
+    generateOutputAudio.set_output_queue(playAudio.input_queue)
 
     # convGen.start()
     # emotionModel.start()
-    # generateOutputAudio.start()
-    # playAudio.start()
-
+    generateOutputAudio.start()
+    playAudio.start()
+    isRunning = True
+    event.clear()
+    
     print("init done")
 
-    isRunning = True
-    # event.clear()
-
-    # ## 메인.while
-    if isRunning:
+    # ## 메인
+    while isRunning:
         
 
         ## 키워드 인식 !! 완료 !!
-        inputHandle.recognize_keyword()
+        # inputHandle.recognize_keyword()
 
         ## wakeup sound 
-        ws.Beep(500, 200)
+        ws.Beep(500, 1000)
         ## TODO : 비프음 대신 대답파일로 교체. 
         ## TODO : arduino serial 신호 전송 (normal)
     
 
     
         ## 유저 음성 받기 !! 완료 !!
-        userInput, user_input_text, user_input_audio = inputHandle.get_user_input(filename='./wav/userSentence.wav',
-                                                                                      inputWaitTIme=10, 
-                                                                                      silence_duration=2, 
-                                                                                      silence_threshold=40)
+        # userInput, user_input_text, user_input_audio = inputHandle.get_user_input(filename='./wav/userSentence.wav',
+        #                                                                               inputWaitTIme=10, 
+        #                                                                               silence_duration=2, 
+        #                                                                               silence_threshold=40)
 
-        ## 오디오 테스트용 
+        ## TTS TEST 
         task = TASK.CONVERATION
         userInput = True
-        user_input_text = "테스트용 인풋"
-        user_input_audio = './wav/ttsTest.wav'
+        user_input_text1 = "테스트용 인풋 입니다."
+        user_input_text2 = "테스트용 인풋 두번째입니다."
+        user_input_text3 = "테스트용 인풋 세번째입니다."
+        
+        ## happy-123, angry-1234, sad-1234, normal-1234
+        generateOutputAudio.push_input(THREAD_STATUS.RUNNING, "happy-3", user_input_text1)
+        generateOutputAudio.push_input(THREAD_STATUS.RUNNING, "happy-3", user_input_text2)
+        generateOutputAudio.push_input(THREAD_STATUS.RUNNING, "happy-3", user_input_text3)
+        generateOutputAudio.push_input(THREAD_STATUS.DONE, "", "")
+
+
         
         ## 유저가 음성을 받았을 때
         if userInput :
-            print("userInput: ", user_input_text, "userInputAudio:", user_input_audio)
-
             # 백그라운드 쓰레드에서 감정분석, 대화생성
             # emotionModel.push_input(THREAD_STATUS.RUNNING, user_input_text, user_input_audio)
             # convGen.push_input(THREAD_STATUS.RUNNING, user_input_text)
-
 
             # 랭체인으로 음성 문자열 작업 분류
             # task = taskClassifier.classify(user_input_text) 
 
             ## 대화로 분류됐을 시 오디오 출력
             ## convGen-> tts, 
-            # if task == TASK.CONVERATION:
-            #     event.set()
-            #     while playAudio.get_status() == THREAD_STATUS.RUNNING:
-            #         pass
-            #     event.clear()
+            if task == TASK.CONVERATION:
+
+                
+                ## 오디오 쓰레드 열어서 음성 출력
+                event.set()
+                while playAudio.get_status() == THREAD_STATUS.RUNNING:
+                    pass
+                event.clear()
+                
+
+                isRunning = False  ## TEST
 
             # elif task == TASK.MUSIC_RECOMMEND:
             #     playAudio.clear_input()
@@ -94,9 +107,10 @@ def main():
             
             # elif 유저시나리오 음악 조작
 
-            isRunning = False
+            # isRunning = False
             # break
 
+    print("over")
     # convGen.push_input(THREAD_STATUS.FINISH, "")
     # convGen.finish()
     # generateOutputAudio.finish()
@@ -112,4 +126,5 @@ if __name__ == "__main__":
     
     
     main()
+    print("over")
 
