e9043ca4e72dc522d9da74f76e1b8f51970402aa
juhyun <kingsjaqj1@naver.com>
2024-05-20 11:50:17 +0900

    x

--- raspi_zzanggu/main.py
+++ raspi_zzanggu/main.py
@@ -1,6 +1,6 @@
 import subprocess, sys
 ## pip install
-# subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", 'requirements.txt'])
+#subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", 'requirements.txt'])
 
 def main():
 
